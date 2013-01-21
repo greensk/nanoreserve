90f102c4af2692ea7a14c13e72a3b788deb55cfb
Grigory Kosheev <green@green>
2013-01-21 19:35:54 +0700

    Utility core extended

--- nanoreserve.py
+++ nanoreserve.py
@@ -19,6 +19,28 @@ config = dict(defaultConfig.items() + config.items())
 tmp = config['options']['tempDir'] + '/' + config['options']['tempPrefix'] + '_' + str(int(time.time()))
 os.mkdir(tmp)
 
-# :TODO: utility code
+files = []
+
+for iId in config['input']:
+	
+	print 'input ' + iId + ':'
+	item = config['input'][iId]
+	
+	if item['type'] == 'fs':
+		# :TODO: fs dump command
+		pass
+	else:
+		print 'Error: unknown type %s' % item['type']
+	
+for oId in config['output']:
+	
+	print 'output ' + oId + ':'
+	item = config['output'][oId]
+	
+	if item['type'] == 'fs':
+		# :TODO: fs copy command
+		pass
+	else:
+		print 'Error: unknown type %s' % item['type']
 
 os.removedirs(tmp)
